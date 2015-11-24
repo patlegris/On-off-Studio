82c2c4d42a0f460899174e84e54ba1b9df8c1350
Ysmaen <lecointre.patrick@laposte.net>
2015-11-24 14:21:40 +0100

    Dates artistes ok

--- wp-content/themes/archi-child/content-video.php
+++ wp-content/themes/archi-child/content-video.php
@@ -5,29 +5,30 @@
         <div class="post-image">
             <iframe height="420" src="<?php echo esc_url($link_video); ?>"></iframe>
         </div>
-        <?php
-        if (has_category()) {
-            $the_cat = get_the_category([0]);
+        <?php if (in_category('artistes')) : ?>
 
-            if ($the_cat <> 'artistes') {
-                $start_date = get_post_meta(get_the_ID(), '_mem_start_date', true);
-                $mem_start_date = date_i18n(get_option('date_format'), strtotime($start_date));
-                $end_date = get_post_meta(get_the_ID(), '_mem_end_date', true);
-                $mem_end_date = date_i18n(get_option('date_format'), strtotime($end_date));
+        <?php else :
+            $start_date = get_post_meta(get_the_ID(), '_mem_start_date', true);
+            $mem_start_date = date_i18n(get_option('date_format'), strtotime($start_date));
+            $end_date = get_post_meta(get_the_ID(), '_mem_end_date', true);
+            $mem_end_date = date_i18n(get_option('date_format'), strtotime($end_date));
 
-                if ($mem_start_date !== "") {
-                    echo '<div class="periode">' . $mem_start_date . ' / ';
-                }
-                if ($mem_end_date !== "") {
-                    echo $mem_end_date . '</div>';
-                }
+            if ($mem_start_date !== "") {
+                echo '
+                                    <div class="periode">' . $mem_start_date . ' / ';
             }
-        }
-        ?>
+            if ($mem_end_date !== "") {
+                echo $mem_end_date . '
+                                    </div>
+                                    ';
+            }
+
+            ?>
+        <?php endif; ?>
 
         <div class="post-text">
             <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <h4>Auteur : <?php the_author();?></h4>
+            <h4><?php the_author();?></h4>
 
 
             <a href="<?php the_permalink(); ?>" class="btn-more"><?php _e('Voir +', 'archi'); ?></a>
